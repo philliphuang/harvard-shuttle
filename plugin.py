f31159af12c5ac8e7a163667c9600d08b6b1492a
Phillip Huang <philliphuang@college.harvard.edu>
2016-09-13 21:42:22 -0400

    quick fix for instant functionality

--- plugin.py
+++ plugin.py
@@ -1,11 +1,20 @@
-def results(fields, original_query):
-  message = fields['~message']
-  return {
-    "title": "Say '{0}'".format(message),
-    "run_args": [message],
-	"html": "<h1 style='font-family: sans-serif; padding: 2em'>{0}</h1>".format(message)
-  }
+import json
 
-def run(message):
-  import os, pipes
-  os.system('say "{0}"'.format(pipes.quote(message.encode('utf8'))))
+def results(parsed, original_query):
+    # target = parsed.values()[0]
+    # print target
+    # if target in ('mather', 'Mather', 'Mather Shuttle'):
+    #     url = 'http://harvard.transloc.com/m/stop/code/113'
+    # else:
+    url = 'http://harvard.transloc.com/m/'
+    return {
+        "title": "Harvard Shuttle",
+        "html": "<script>window.location=%s</script>" % json.dumps(url),
+        # "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
+        # "webview_links_open_in_browser": True,
+        "run_args": [url]
+    }
+
+def run(url):
+    import os
+    os.system('open "{0}"'.format(url))
