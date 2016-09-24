93503838a229c330837c0f50f5aeafbae0a23403
Phillip Huang <philliphuang@college.harvard.edu>
2016-09-23 22:32:33 -0400

    added mather option & icon

--- plugin.py
+++ plugin.py
@@ -6,9 +6,14 @@ def results(parsed, original_query):
     # if target in ('mather', 'Mather', 'Mather Shuttle'):
     #     url = 'http://harvard.transloc.com/m/stop/code/113'
     # else:
-    url = 'http://harvard.transloc.com/m/'
+    if original_query == 'mather':
+        url = 'http://harvard.transloc.com/m/stop/code/113'
+        title = 'Harvard Shuttle - Mather'
+    else:
+        url = 'http://harvard.transloc.com/m/'
+        title = 'Harvard Shuttle'
     return {
-        "title": "Harvard Shuttle",
+        "title": title,
         "html": "<script>window.location=%s</script>" % json.dumps(url),
         # "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
         # "webview_links_open_in_browser": True,
